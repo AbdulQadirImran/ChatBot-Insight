0c151b302b2268e66b9966e0168e7d614705a21d
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-06-02 22:48:38 +0500

    Update layout.tsx

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,7 +1,7 @@
 import { SidebarDesktop } from '@/components/sidebar-desktop'
 
 interface ChatLayoutProps { 
-  children: React.ReactNode
+  children: React.ReactNode 
 }
 
 export default async function ChatLayout({ children }: ChatLayoutProps) {
