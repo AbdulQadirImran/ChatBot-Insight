c202d94e0e2a87b49c334017f0bb89fcc33b6e1a
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-06-02 22:38:56 +0500

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -6,7 +6,7 @@ import { kv } from '@vercel/kv'
  
 import { auth } from '@/auth'
 import { type Chat } from '@/lib/types'
-
+ 
 export async function getChats(userId?: string | null) {
   if (!userId) {
     return []
