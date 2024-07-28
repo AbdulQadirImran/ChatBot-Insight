c3a8ba1f405ccc652c8107785377d1b54408cb0f
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-07-28 07:22:22 +0500

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth'                
+import NextAuth from 'next-auth'                 
 import Credentials from 'next-auth/providers/credentials'
 import { authConfig } from './auth.config'
 import { z } from 'zod'
